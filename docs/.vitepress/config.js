c1b81fd8ba6e686abf8e9736842bf671ab40d739
Starlong <starlong.lu@gmail.com>
2022-04-01 22:52:16 +0800

    hello vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'Starlong',
+  description: 'Just playing around.',
+  
+  themeConfig: {
+    // Type is `DefaultTheme.Config`
+  }
+})
